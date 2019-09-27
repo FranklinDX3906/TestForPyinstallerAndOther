8e46bf7c060e43253a0b57e6051eb46049e98723
张毅恒 <993628121@qq.com>
2019-09-27 13:17:01 +0800

    Change some code and create a new exe.

--- TestForPyinstallerAndOther.py
+++ TestForPyinstallerAndOther.py
@@ -1,6 +1,7 @@
 #coding:UTF-8
 
 import tkinter as tk
+from numpy.distutils.cpuinfo import command_by_line
 
 class HiCBB:
     def __init__(self,root):
@@ -12,16 +13,19 @@ class HiCBB:
     def Test(self):
         def say_hi():
             TextShow.set("你好啊，曹不不")
+            next['state'] = 'disabled'
+            Quit = tk.Button(self.frame2,text = "退出",fg = "black",command = root.quit)#产生一个新的按钮
+            Quit.pack()
                 
         self.frame1.pack()#自动调整窗口大小
         self.frame2.pack()
         TextShow = tk.StringVar()#创建一个字段
         TextShow.set("我是DX3906")
-        TextLabel = tk.Label(self.frame1, textvariable = TextShow,justify = tk.LEFT,font = 'Helvetica -30 bold italic')#使用一个label展示字段
+        TextLabel = tk.Label(self.frame1, textvariable = TextShow,justify = tk.LEFT,font = 'Helvetica -30 bold')#使用一个label展示字段
         TextLabel.pack(padx=100,pady = 100)
         
-        self.next =  tk.Button(self.frame2, text = "233333", fg="black",command = say_hi)
-        self.next.pack()
+        next =  tk.Button(self.frame2, text = "233333", fg="black",command = say_hi)
+        next.pack()
         
 
         
@@ -32,29 +36,3 @@ Test = HiCBB(root)
 Test.Test()
 
 root.mainloop()
-
-'''        
-from tkinter import *
-
-def callback():
-    var.set("吹吧你，我才不信嘞！")
-
-root = Tk()
-frame1 = Frame(root)
-frame2 = Frame(root)
-# 创建一个文本Label对象
-var = StringVar()
-var.set("您所下载的影片含有未成年人限制内容，\n请满18岁后再点击观看！")
-textLabel = Label(frame1, textvariable=var, justify=LEFT)
-textLabel.pack(side=LEFT)
-# 创建一个图形Label对象
-# 用PhotoImage实例化一个图片对象(支持gif格式文件哦)
-# 加一个按钮
-theButton = Button(frame2, text="已满18周岁", command=callback)
-theButton.pack()
-frame1.pack(padx=10, pady=10)
-frame2.pack(padx=10, pady=10)
-
-mainloop()
-
-'''
